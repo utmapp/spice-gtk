f5b9472449fca5cba417ea8ada5194e8c28ceedc
osy <50960678+osy@users.noreply.github.com>
2019-04-24 18:44:05 -0700

    coroutine: added support for libucontext

--- src/continuation.h
+++ src/continuation.h
@@ -23,7 +23,16 @@
 
 #include "spice-common.h"
 #include <stddef.h>
+#ifdef HAVE_LIBUCONTEXT
+#include <libucontext/libucontext.h>
+#define ucontext_t libucontext_ucontext_t
+#define getcontext libucontext_getcontext
+#define setcontext libucontext_setcontext
+#define swapcontext libucontext_swapcontext
+#define makecontext libucontext_makecontext
+#else
 #include <ucontext.h>
+#endif
 #include <setjmp.h>
 
 struct continuation
